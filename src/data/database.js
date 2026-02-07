4306a9f8060e48e7c1d9f52227218e2f8f52a725
dmgproductionsoficial <dmgproductionsoficial@gmail.com>
2026-02-07 14:20:55 +0000

    sync

--- src/data/database.js
+++ src/data/database.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const config = require('../../config/config.json');
 
-const env = process.env.NODE_ENV || 'development';
-const dbConfig = config[env];
-
-const sequelize = new Sequelize(dbConfig);
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'postgres',
+});
 
 module.exports = sequelize;
